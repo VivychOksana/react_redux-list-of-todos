520fcd1631e4e0506e9f679b80cf6fbc00203413
Vivych Oksana <vivichokcana@gmail.com>
2020-01-09 23:30:28 +0200

    Updates

---
+++ precache-manifest.433a3e0c8c91d068550cf4d8004a008c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2711af28bdde0413fad558744563ad07",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "113b2523c638d699fd4b",
+    "url": "/react_redux-list-of-todos/static/css/main.eaac793d.chunk.css"
+  },
+  {
+    "revision": "7093c5096a3e9842d351",
+    "url": "/react_redux-list-of-todos/static/js/2.7e300f46.chunk.js"
+  },
+  {
+    "revision": "113b2523c638d699fd4b",
+    "url": "/react_redux-list-of-todos/static/js/main.3ce73130.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
